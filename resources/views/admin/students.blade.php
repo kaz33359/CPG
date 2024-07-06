d649f4f68fad5bb6274001039db23d20188b4178
Ashwin K <88078602+Ash3131@users.noreply.github.com>
2024-07-07 00:57:00 +0530

    version_updates
    upgraded to latest version

--- resources/views/admin/students.blade.php
+++ resources/views/admin/students.blade.php
@@ -297,7 +297,7 @@
                 <div class="page-header">
                     <nav class="breadcrumb-one" aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="javascript:void(0);">Dashboad</a></li>
+                            <li class="breadcrumb-item"><a href="javascript:void(0);">Dashboard</a></li>
                             <li class="breadcrumb-item active" aria-current="page"><a
                                     href="javascript:void(0);">Student</a></li>
                         </ol>
